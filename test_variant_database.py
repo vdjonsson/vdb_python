8f1ba3dda59efb0f92ae3223e44727c4ef4d152f
Vanessa Jonsson <41131504+vdjonsson@users.noreply.github.com>
2022-05-20 17:37:47 -0700

    Updated singleton code and test

--- test_variant_database.py
+++ test_variant_database.py
@@ -1,15 +1,17 @@
 import VariantDatabase as vd
 
-
-
 path = '/Users/vanessajonsson/Google\ Drive/data/rep/vdb-main/v27_new'
 working_dir = '/Users/vanessajonsson/Google Drive/data/rep/vdb-main/'
 
 vdb = vd.VariantDatabase(path, working_dir)
-
 data = vdb.command(command = 'from', args= 'US', cluster_name = 'x',saveto='usa.csv')
 
 print(data)
+
+''' Get instance test '''
+vdb_instance = vd.VariantDatabase.getInstance()
+
+print('Path of the Variant Database Singleton:', vdb_instance.path)
 
 ''' Singleton class test '''
 vdb_error = vd.VariantDatabase(path, working_dir)
