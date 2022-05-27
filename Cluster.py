fd0ea3d1020e61b626ed6e381ba148015dae9eeb
Vanessa Jonsson <41131504+vdjonsson@users.noreply.github.com>
2022-05-26 22:02:13 -0700

    Add files via upload
    Added filtering functionality and tests

--- Cluster.py
+++ Cluster.py
@@ -10,13 +10,13 @@ class Cluster:
     def __init__(self, command:str, cluster_name:str='', verbose=None):
         self.command = command
         self.vdb = vd.VariantDatabase()
-        self.cluster_name = cluster_name
-
+        self.cluster_name = cluster_name # APW
+        self.verbose = False # APW 
         if verbose == None:
             if self.vdb.verbose == True:
-                verbose = True
+                self.verbose = True
             if self.vdb.verbose == False:
-                verbose = False
+                self.verbose = False
 
         if len(cluster_name) == 0:
             nameFromDeclaration = self.clusterNameFromDeclaration()
@@ -30,7 +30,7 @@ class Cluster:
 
         self.data = self.vdb.command(command)
 
-        if verbose:
+        if self.verbose:
             print(self.data)
              
         self.lineages_data = None
@@ -40,9 +40,20 @@ class Cluster:
         self.weekly_data = None
         self.patterns_data = None
         self.consensus_data = None 
-        #self.variants_data = None
+        self.subclusters = dict()
            
 
+    def info(self):
+        print('---- Cluster ----')
+        print('Name:', self.cluster_name)
+        print('Definition:', self.command)
+        print('Num. clusters:', len(self.subclusters))
+
+        if len(self.subclusters) > 0 :
+            print('---- Subcluster ----')
+            for c in self.subclusters.values():
+                c.info()
+            
     def clusterNameFromDeclaration(self):
         previous_frame = inspect.currentframe().f_back.f_back
         (_,_,_,lines,_) = inspect.getframeinfo(previous_frame)
@@ -193,7 +204,8 @@ class Cluster:
 
     def plot (self,command='from', groupby=None, logy=False, top=5):
 
-        title = command + ', cluster:' + self.command #+ ',top: ' + str(top)
+        title = command + ', Cluster:' + self.cluster_name + ' Command:'+ self.command
+        
         if command == 'from':
             # Todo implement heatmap of mutations per virus
 
@@ -298,28 +310,160 @@ class Cluster:
         plt.title(title)
 
     ''' Filtering commands ''' 
-    def containing(self, pattern:list=None):
-        return 
+    def containing(self, pattern:list,str=None):
 
-    def notcontaining(self, pattern:list=None):
-        return
+        pattern_str = pattern
+
+        if isinstance(pattern, list):
+            pattern_str = ''
+            for p in pattern:
+                pattern_str = pattern_str + p + ' '
+
+        command = self.cluster_name + ' containing ' + pattern_str
+        cluster = Cluster(command = command)
+        #self.subclusters[cluster.cluster_name] = cluster #APW
+        
+        if self.verbose:
+            print(command)
+            print(cluster.data)
+            
+        return cluster
+
+    def notcontaining(self, pattern:list,str=None):
+
+        pattern_str = pattern
+
+        if isinstance(pattern, list):
+            pattern_str = ''
+            for p in pattern:
+                pattern_str = pattern_str + p + ' '
+
+        command = self.cluster_name + ' not containing ' + pattern_str
+        cluster = Cluster(command = command)
+        #self.subclusters[cluster.cluster_name] = cluster
+        
+        if self.verbose:
+            print(command)
+            print(cluster.data)
+            
+        return cluster
+
 
     def before(self, date:str=None):
-        return
+
+        command = self.cluster_name + ' before ' + date
+        print(command)
+        cluster = Cluster(command = command)
+        #self.subclusters[cluster.cluster_name] = cluster
+        # Create new cluster here and return it
+        if self.verbose:
+            print(command)
+            print(cluster.data)
+            
+        return cluster
+
 
     def after(self, date:str=None):
-        return
+        
+        command = self.cluster_name + ' after ' + date
+        print(command) 
+        cluster = Cluster(command = command)
+        #self.subclusters[cluster.cluster_name] = cluster
+        # Create new cluster here and return it
+        if self.verbose:
+            print(command)
+            print(cluster.data)
+            
+        return cluster
 
-    def range(self, dates:str=None):
-        return
+
+    def range(self, dates:list,str=None):
+        pattern_str = dates
+
+        if isinstance(dates, list):
+            pattern_str = ''
+            for p in dates:
+                pattern_str = pattern_str + p + '-'
+
+            pattern_str=pattern_str[:-1]
+
+        command = self.cluster_name + ' range ' + pattern_str
+        print('COMMAND')
+        print(command)
+        cluster = Cluster(command = command)
+        #self.subclusters[cluster.cluster_name] = cluster
+        
+        if self.verbose:
+            print(command)
+            print(cluster.data)
+            
+        return cluster
+
 
     def lessthan(self, num_mutations:int=None):
-        return
+        
+        command = self.cluster_name + ' < ' + num_mutations
+        cluster = Cluster(command = command)
+        #self.subclusters[cluster.cluster_name] = cluster
+        if self.verbose:
+            print(command)
+            print(cluster.data)
+            
+        return cluster
+
     
     def morethan(self, num_mutations:int=None):
-        return
+        
+        command = self.cluster_name + ' > ' + num_mutations
+        
+        cluster = Cluster(command = command)
+        #self.subclusters[cluster.cluster_name] = cluster
+        if self.verbose:
+            print(command)
+            print(cluster.data)
+            
+        return cluster
 
-    def sample(self, num_mutations:int=None):
-        return 
- 
+    def named(self, state_id:str=None, epi_id:str=None) :
+        arg= state_id 
+        if state_id == None:
+            arg = epi_id
+            
+        command = self.cluster_name + ' named ' + arg
+        cluster = Cluster(command = command)
+        #self.subclusters[cluster.cluster_name] = cluster
+        
+        if self.verbose:
+            print(command)
+            print(cluster.data)
+            
+        return cluster
+
+
+    def lineage(self, pango_lineage:str):
+        
+        command = self.cluster_name + ' lineage ' + pango_lineage
+        cluster = Cluster(command = command)
+        #self.subclusters[cluster.cluster_name] = cluster
+        
+        if self.verbose:
+            print(command)
+            print(cluster.data)
+            
+        return cluster
+
+    def sample(self, fraction:float=None):
+
+        command = self.cluster_name + ' sample ' + str(fraction)
+        cluster = Cluster(command = command)
+        
+        #self.subclusters[cluster.cluster_name] = cluster
+
+        if self.verbose:
+            print(command)
+            print(cluster.data)
+            
+        return cluster
+
+
 
--- test_cluster.py
+++ test_cluster.py
@@ -3,20 +3,76 @@ import matplotlib.pyplot as plt
 
 
 ''' Unit test for this class ''' 
-x = c.Cluster("from ca",verbose=True)
+x = c.Cluster("from ca",cluster_name ='x',verbose=True)
 y = c.Cluster("x after 12/1/20",verbose=True)
 z = c.Cluster("B.1.1.7 from ny before 10/1/22",verbose=True)
 a = c.Cluster('from France + Japan', verbose=True)
+z= c.Cluster('from US', verbose=True)
+
+x.info()
 
 
-a.countries()
-print(a.countries_data)
-a.plot('countries',logy=True)
+#a.countries()
+#print(a.countries_data)
+#a.plot('countries',logy=True)
+#plt.tight_layout()
+#plt.show()
+
+#a.frequency()
+#print(a.freq_data)
+#a.plot('freq',logy=True, top=10)
+#plt.tight_layout()
+#plt.show()
+
+nca = a.notcontaining('E484K N501Y') # OR nca = a.notcontaining(['E484K', 'N501Y'])
+print(nca)
+nca.info()
+
+
+ca = a.containing('E484K N501Y') # OR ca = a.containing(['E484K', 'N501Y'])
+
+ca.frequency()
+print(ca.freq_data) 
+ca.plot('freq',logy=True, top=10)
 plt.tight_layout()
 plt.show()
 
+
+#s = z.named('CA') #APW 
+s = z.lineage('B.1.1')
+
+
+z.info() 
+
+z.trends()
+z.plot('trends',logy=True, top=5)
+plt.tight_layout()
+plt.show()
+
+
+s = z.sample(0.38)
+s.trends()
+s.plot('trends', logy=True, top=5)
+s.info()
+
+s.frequency()
+s.plot('freq',logy=True, top=10)
+plt.tight_layout()
+plt.show()
+
+
+aa = a.range('6/1/2020-8/1/2020') # OR
+s = a.range(['6/1/2020','8/1/2020'])
+
+o = a.after('4/1/2022')
+o.frequency()
+o.plot('freq',logy=True, top=10)
+plt.tight_layout()
+plt.show()
+
+
+
 a.states()
-print(a.states_data)
 a.plot('states',logy=True)
 plt.tight_layout()
 plt.show()
@@ -38,7 +94,6 @@ print(a.monthly_data)
 a.plot('monthly',logy=True)
 plt.tight_layout()
 plt.show()
-
 
 y.lineages()
 print(y.lineages_data)
