8def2fa95309d740dc74c48f441b69d0ba2639b2
Anthony West <56272592+anthonypwest@users.noreply.github.com>
2022-05-28 07:54:57 -0700

    useful shorthands for various commands

---
+++ setup.py
@@ -0,0 +1,10 @@
+import VariantDatabase as vd
+import Cluster
+cl = Cluster.Cluster
+pat = Cluster.pat.Pattern
+v = vd.v
+V = vd.V
+vdb = vd.VariantDatabase()
+vdb.verbose = True
+world = cl("world")
+diff = Cluster.diff
